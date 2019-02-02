9cc21dd5f1a1fc30d03f388b4d5e7d309ae69d48
Addison Wojciechowski <47170545+Verlieren@users.noreply.github.com>
2019-02-02 05:13:20 -0500

    added multiple functions

--- GFLPBot/GFLPBot.py
+++ GFLPBot/GFLPBot.py
@@ -1,6 +1,7 @@
 import discord
 from discord import *
 from discord.ext.commands import Bot
+from discord.utils import get
 
 PREFIX = "$"
 INVITELINK = "https://discord.gg/VnbX8JH"
@@ -35,7 +36,6 @@ class GFLPBot:
 
         @self.client.event
         async def on_message(message):
-            flagged = False
             warning_message = "Please use the #voice-text channel when you are connected to voice." + \
                               "This reduces clutter for other users and makes" + \
                               " it easier for them to follow the chat."
@@ -45,24 +45,28 @@ class GFLPBot:
                 if message.author.bot:
                     return
 
-                for roles in message.author.roles:
-                    if roles.name == "Non-Vocalizer":
-                        flagged = True
+                role = get(message.author.roles, name="Non-Vocalizer")
 
-                if flagged and message.channel.name != "voice-text" and message.author.voice.self_mute \
+                if message.channel.name != "voice-text" and role is not None and message.author.voice.self_mute \
                         and message.author.voice.voice_channel is not None:
                     await self.client.delete_message(message)
                     print("\n")
                     print("Message Deleted:")
                     print(message.content)
                     print(message.author)
+                    print(message.timestamp)
                     try:
                         await self.client.send_message(destination=message.author, content=warning_message)
                     except discord.errors.Forbidden:
                         print("User has blocked bot, no DM sent")
+                    return
 
-                if message.content.startswith(PREFIX):
-                    await self.client.process_commands(message)
+            if "o tru" in message.content:
+                emoji = get(self.client.get_all_emojis(), name='otru')
+                await self.client.add_reaction(message, emoji)
+
+            if message.content.startswith(PREFIX):
+                await self.client.process_commands(message)
 
         @self.client.command(name='invite')
         async def create_invite():
@@ -70,3 +74,14 @@ class GFLPBot:
             response = "Permanent invite link for GainFatLP:\n" + INVITELINK
             await self.client.say(response)
 
+        @self.client.command(name='ongod',
+                             pass_context=True)
+        async def on_god(ctx):
+            with open("..\\Images\\Misc\\on_god.png", "rb") as f:
+                await self.client.send_file(ctx.message.channel, f)
+
+        @self.client.command(name='ok',
+                             pass_context=True)
+        async def ok(ctx):
+            with open("..\\Images\\Misc\\ok.jpg", "rb") as f:
+                await self.client.send_file(ctx.message.channel, f)
